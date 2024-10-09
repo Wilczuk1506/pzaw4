b8aeb3ef6ee89c8976470604abb331cb433cf919
wilczuk1506 <Wilczuk1506@DOM.SCI.EDU.PL>
2024-10-09 08:28:01 +0200

    09.10.2024_1

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import Sci from './Sci';
+import Temp from './Temp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Sci />
+    <Temp />
   </React.StrictMode>
 );
