768d3409f390d4385bec776d63b308195b90b9c0
test <test@DOM.SCI.EDU.PL>
2024-12-02 16:38:20 +0100

    task3 02.12.2024

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
-import cors from "cors";
+const cors = require('cors');
+const fs = require('fs');
 
 const application = express();
 
 application.use(cors())
 application.use(express.json())
 
-import pokedex from "./pokedex/pokedex.json";
-import types from "./pokedex/types.json";
-
 let arr = []
-
 
 application.get('/task02/data', (req, res) => {
     res.status(200).json(arr);
@@ -23,29 +20,39 @@ application.post('/task02/data', (req, res) => {
     res.sendStatus(200);
 });
 
+// Pokedex
+
+function base64_encode(file) {
+    var bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
+}
+
+const types = require("./pokedex/types.json");
+const pokedex = require("./pokedex/pokedex.json");
+
 const task3_router = express.Router();
 
 task3_router.get("/types", (req, res) => {
-
+    res.status(200).json(types);
 });
 
-task3_router.get("/", (req, res) => {
-    pokemons[req.params.id]
+task3_router.get("/:id", (req, res) => {
+    res.status(200).json(pokedex.find((x) => x.id === Number(req.params.id)));
 });
 
-task3_router.get("/types", (req, res) => {
-    
+task3_router.get("/type/:type", (req, res) => {
+    res.status(200).json(pokedex.filter(pokemon => pokemon.type.includes(req.params.type)));
 });
 
-task3_router.get("/imgs", (req, res) => {
-    
+task3_router.get("/image/:id", (req, res) => {
+    res.status(200).send(base64_encode(`./pokedex/images/${(req.params.id).padStart(3, '0')}.png`));
 });
 
 task3_router.get("", (req, res) => {
-    
+    res.status(200).json(pokedex.filter(pokemon => req.query.types.every(type => pokemon.type.includes(type))));
 });
 
-task3.use("/task3/pokemon", task3_router);
+application.use("/task3/pokemon", task3_router);
 
 
 application.listen(8000, () => {console.log("server status")});
--- frontend/src/Task3/Task3.js
+++ frontend/src/Task3/Task3.js
@@ -1,7 +1,97 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function Task3(){
+    const [pokemonTypes, setPokemonTypes] = useState([]);
+
+    const [pokemonId, setPokemonId] = useState(Number);
+    const [pokemonFromId, setPokemonFromId] = useState({});
+    const [imgUrlFromId, setImgUrlFromId] = useState();
+
+    const [pokemonType, setPokemonType] = useState(String);
+    const [pokemonsFromType, setPokemonsFromType] = useState([])
+
+    
+    //Pokemon types
+    useEffect(() => {
+        setPokemonId(1);
+        setPokemonType("Fire");
+
+        axios.get("http://localhost:8000/task3/pokemon/types")
+        .then((response) => {
+            setPokemonTypes(response.data);
+        })
+        .catch((error) => {
+            console.error(":c", error);
+        })
+    }, [pokemonType]);
+
+    //Pokemon from id
+    useEffect(() => {
+        //info
+        axios.get(`http://localhost:8000/task3/pokemon/${pokemonId}`)
+        .then((response) => {
+            setPokemonFromId(response.data);
+        })
+        .catch((error) => {
+            console.error(":c", error);
+        })
+
+        //img
+        axios.get(`http://localhost:8000/task3/pokemon/image/3`)
+        .then((response) => {
+            setImgUrlFromId(response.data);
+        })
+        .catch((error) => {
+            console.error(":c", error);
+        })
+    }, [pokemonId]);
+
+    //Pokemons from type
+    useEffect(() => {
+        axios.get(`http://localhost:8000/task3/pokemon/type/${pokemonType}`)
+        .then((response) => {
+            if (typeof(response.data) !== "string")
+                setPokemonsFromType(response.data);
+        })
+        .catch((error) => {
+            console.error(":c", error);
+        })
+    }, [pokemonType]);
+
     return (
         <div>
-            <button type="button" className="btn btn-primary">Primary</button>
+            {/* pokemon types */}
+            <div>
+                {
+                    pokemonTypes.map((e, index) => {
+                        return (
+                            e.english
+                        )
+                    })
+                }
+            </div>
+
+            {/* from type */}
+            <div>
+                {
+                    pokemonsFromType.map((e, index) => {
+                        return (
+                            e.name.english
+                        )
+                    })
+                }
+            </div>
+
+            {/* from id */}
+            <div>
+                {
+                    pokemonFromId?.name?.english
+                }
+                {
+                    imgUrlFromId ? ( <img src={`data:image/png;base64, ${imgUrlFromId}`} alt='oopsie'/> ) : null
+                }
+            </div>
         </div>
     );
 }
